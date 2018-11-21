eb79728739381e8d5d3f8f7a538c0c45a28cf735
HassakuTb <hassaku.sc@gmail.com>
2018-11-21 13:43:59 +0900

    add ForEach extension

---
+++ Assets/Citrus/Scripts/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Citrus {
+    /// <summary>
+    /// IEnumerable<>拡張
+    /// </summary>
+    public static class IEnumerableExtensions {
+
+        /// <summary>
+        /// 全ての要素についてactionを実行する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elements"></param>
+        /// <param name="action"></param>
+        public static void ForEach<T> (this IEnumerable<T> elements, Action<T> action)
+        {
+            foreach(T e in elements)
+            {
+                action(e);
+            }
+        }
+    }
+}
