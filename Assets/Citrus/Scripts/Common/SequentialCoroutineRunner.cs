13b360304b2c41aa47fbe6e07db63b32329e7133
HassakuTb <hassaku.sc@gmail.com>
2018-12-06 15:22:39 +0900

    SequentialCoroutineRunnerをタイプセーフにして機能を追加

--- Assets/Citrus/Scripts/Common/SequentialCoroutineRunner.cs
+++ Assets/Citrus/Scripts/Common/SequentialCoroutineRunner.cs
@@ -4,21 +4,115 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Citrus {
+
+    public enum CoroutineRunnerErrorType {
+        RaisedError,
+        Exception,
+        Timeout,
+    }
+
+    /// <summary>
+    /// CoroutineRunnerの中で発生するエラー
+    /// </summary>
+    /// <typeparam name="RaisedErrorType">アプリケーション側で取り扱うエラーの型</typeparam>
+    public class CoroutineRunnerError<RaisedErrorType> {
+
+        /// <summary>
+        /// ユーザーが発生させたエラーを生成する
+        /// </summary>
+        /// <typeparam name="T">アプリケーション側で取り扱うエラーの型</typeparam>
+        /// <param name="raisedError">発生させるエラー</param>
+        /// <returns></returns>
+        internal static CoroutineRunnerError<T> CreateRaisedError<T>(T raisedError)
+        {
+            CoroutineRunnerError<T> error = new CoroutineRunnerError<T>
+            {
+                Type = CoroutineRunnerErrorType.RaisedError,
+                RaisedError = raisedError
+            };
+            return error;
+        }
+
+        /// <summary>
+        /// 実行中に発生した例外を生成する
+        /// </summary>
+        /// <typeparam name="T">アプリケーション側で取り扱うエラーの型</typeparam>
+        /// <param name="exception">発生した例外</param>
+        /// <returns></returns>
+        internal static CoroutineRunnerError<T> CreateExceptionError<T>(Exception exception)
+        {
+            CoroutineRunnerError<T> error = new CoroutineRunnerError<T>
+            {
+                Type = CoroutineRunnerErrorType.Exception,
+                Exception = exception
+            };
+            return error;
+        }
+
+        /// <summary>
+        /// タイムアウトエラーを生成する
+        /// </summary>
+        /// <typeparam name="T">アプリケーション側で取り扱うエラーの型</typeparam>
+        /// <returns></returns>
+        internal static CoroutineRunnerError<T> CreateTimeout<T>()
+        {
+            CoroutineRunnerError<T> error = new CoroutineRunnerError<T>
+            {
+                Type = CoroutineRunnerErrorType.Timeout,
+            };
+            return error;
+        }
+
+        /// <summary>
+        /// エラーがどういうエラーかを取得する
+        /// RaisedError : アプリケーション側でRaiseErrorで発生させたエラー
+        /// Exception : 実行中で発生した例外
+        /// Timeout : タイムアウトした場合
+        /// </summary>
+        public CoroutineRunnerErrorType Type { get; private set; }
+
+        /// <summary>
+        /// アプリケーション側でRaiseErrorで発生させたエラーを取得する
+        /// </summary>
+        /// <remarks>
+        /// Type == CoroutineRunnerErrorType.RaisedError でないときの値は不定
+        /// </remarks>
+        public RaisedErrorType RaisedError { get; private set; }
+
+        /// <summary>
+        /// 実行中に発生した例外を取得する
+        /// </summary>
+        /// <remarks>
+        /// Type == CoroutineRunnerErrorType.Exception でないときの値は不定
+        /// </remarks>
+        public Exception Exception { get; private set; }
+
+        private CoroutineRunnerError() { }
+    }
+
     /// <summary>
     /// 連続したコルーチンを実行する
+    /// このクラスは使いまわしを想定しておらず、使い捨てにする
     /// </summary>
     /// <example>
-    /// var runner = SequentialCoroutineRunner();
-    /// runnner.Append(SomeCoroutine(runner), SomeCoroutine(runner), ...);
+    /// var runner = SequentialCoroutineRunner<ErrorType>();
+    /// runner.Append(SomeCoroutine(runner), SomeCoroutine(runner), ...);
+    /// runner.SetTimeout(30f); //  タイムアウト設定(optional)
+    /// runner.SetInterruption(() => {return ...}, () =>{runner.RaiseError(...);});  // 割り込み設定(optional)
     /// StartCoroutine(runner.Coroutine());
     /// 
-    /// IEnumerator SomeCoroutine(SequentialCoroutineRunner runner)
+    /// IEnumerator SomeCoroutine()
     /// {
     ///     ...
-    ///     runner.Terminate(); //  中断
+    ///     runner.Cancel(); //  中断
+    ///     
+    ///     runner.Append(...); //  処理の追加
+    ///     
+    ///     runner.RaiseError(error);   //  エラー終了
+    ///     
     /// }
     /// </example>
-    public class SequentialCoroutineRunner {
+    public class SequentialCoroutineRunner<ErrorType> {
 
         //  コルーチンキュー
         private Queue<IEnumerator> queue = new Queue<IEnumerator>();
@@ -30,27 +124,33 @@ namespace Citrus {
         private bool isRaisedError = false;
 
         /// <summary>
-        /// Raiseされたエラーを取得する
+        /// 発生したエラーを取得する
         /// </summary>
-        public object Error { get; private set; }
+        public CoroutineRunnerError<ErrorType> Error { get; private set; }
 
         //  完了時処理
         private readonly Action onComplete;
 
         //  エラー時処理
-        private readonly Action<object> onError;
+        private readonly Action<CoroutineRunnerError<ErrorType>> onError;
+
+        //  共通処理
+        private readonly Action onFinally;
 
         //  タイムアウト時間
         private float timeout;
+
+        //  割り込み条件
+        private Func<bool> interruptPredicate;
+
+        //  割り込み時の処理
+        private Action onInterrupt;
 
         /// <summary>
         /// タイムアウトがあるか取得する
         /// デフォルトタイムアウトはなし
         /// </summary>
         public bool HasTimeout { get; private set; }
-
-        //  タイムアウト時処理
-        private Action onTimeout;
 
         /// <summary>
         /// 連続したコルーチンを処理するインスタンスを生成する
@@ -62,10 +162,12 @@ namespace Citrus {
         /// </summary>
         /// <param name="onComplete">成功時の処理</param>
         /// <param name="onError">失敗時の処理</param>
-        public SequentialCoroutineRunner(Action onComplete, Action<object> onError)
+        /// <param name="onFinally">成功、失敗にかかわらず実行する処理</param>
+        public SequentialCoroutineRunner(Action onComplete, Action<CoroutineRunnerError<ErrorType>> onError, Action onFinally = null)
         {
             this.onComplete = onComplete;
             this.onError = onError;
+            this.onFinally = onFinally;
         }
 
         /// <summary>
@@ -74,8 +176,9 @@ namespace Citrus {
         /// <param name="coroutines">処理するコルーチン</param>
         /// <param name="onComplete">成功時の処理</param>
         /// <param name="onError">失敗時の処理</param>
-        public SequentialCoroutineRunner(IEnumerable<IEnumerator> coroutines, Action onComplete, Action<object> onError)
-            : this(onComplete, onError)
+        /// <param name="onFinally">成功、失敗にかかわらず実行する処理</param>
+        public SequentialCoroutineRunner(IEnumerable<IEnumerator> coroutines, Action onComplete, Action<CoroutineRunnerError<ErrorType>> onError, Action onFinally = null)
+            : this(onComplete, onError, onFinally)
         {
             Append(coroutines);
         }
@@ -84,12 +187,24 @@ namespace Citrus {
         /// タイムアウトを設定する
         /// </summary>
         /// <param name="time">タイムアウト時間</param>
-        /// <param name="onTimeout">タイムアウト時の処理</param>
-        public void SetTimeout(float time, Action onTimeout = null)
+        public void SetTimeout(float time)
         {
             HasTimeout = true;
-            this.onTimeout = onTimeout;
             timeout = time;
+        }
+
+        /// <summary>
+        /// 割り込み関数を設定する
+        /// </summary>
+        /// <remarks>
+        /// predicateがtrueのとき、onInterruptが実行される
+        /// </remarks>
+        /// <param name="predicate">割り込み条件/param>
+        /// <param name="onInterrupt">割り込み時に実行される処理</param>
+        public void SetInterruption(Func<bool> predicate, Action onInterrupt)
+        {
+            interruptPredicate = predicate;
+            this.onInterrupt = onInterrupt;
         }
 
         /// <summary>
@@ -155,9 +270,9 @@ namespace Citrus {
         /// }
         /// </example>
         /// <param name="error">発生させるエラー デフォルトはnull</param>
-        public void RaiseError(object error = null)
+        public void RaiseError(ErrorType error)
         {
-            Error = error;
+            Error = CoroutineRunnerError<ErrorType>.CreateRaisedError(error);
             isRequestedCancel = true;
             isRaisedError = true;
         }
@@ -167,8 +282,9 @@ namespace Citrus {
         /// </summary>
         /// <remarks>
         /// 処理を中断した場合、onCompleteとonErrorは両方とも呼び出されない
+        /// onFinallyは呼び出される
         /// </remarks>
-        public void Terminate()
+        public void Cancel()
         {
             isRequestedCancel = true;
         }
@@ -185,29 +301,38 @@ namespace Citrus {
             {
                 if (HasTimeout && Time.realtimeSinceStartup - startTime > timeout)
                 {
-                    onTimeout?.Invoke();
-                    yield break;
+                    Error = CoroutineRunnerError<ErrorType>.CreateTimeout<ErrorType>();
+                    break;
                 }
 
-                if (isRaisedError)
+                if (interruptPredicate != null && interruptPredicate())
                 {
-                    onError?.Invoke(Error);
-                    yield break;
+                    onInterrupt?.Invoke();
                 }
 
-                if (isRequestedCancel)
+                if (isRaisedError || isRequestedCancel)
                 {
-                    yield break;
+                    break;
                 }
 
                 if (queue.Count == 0)
                 {
                     onComplete?.Invoke();
-                    yield break;
+                    break;
                 }
 
                 IEnumerator peek = queue.Peek();
-                if (peek.MoveNext())
+                bool hasNext = false;
+                try
+                {
+                    hasNext = peek.MoveNext();
+                }
+                catch (Exception e)
+                {
+                    Error = CoroutineRunnerError<ErrorType>.CreateExceptionError<ErrorType>(e);
+                    break;
+                }
+                if (hasNext)
                 {
                     yield return peek.Current;
                 }
@@ -216,6 +341,13 @@ namespace Citrus {
                     queue.Dequeue();
                 }
             }
+
+            if (Error != null)
+            {
+                onError?.Invoke(Error);
+            }
+
+            onFinally?.Invoke();
         }
     }
 }
