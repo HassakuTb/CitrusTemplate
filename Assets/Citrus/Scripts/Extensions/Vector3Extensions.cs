ef3697b2428ac41d9b8e721d5ac6af8429433599
HassakuTb <hassaku.sc@gmail.com>
2018-12-06 15:29:15 +0900

    Vector3の拡張を追加

---
+++ Assets/Citrus/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Citrus {
+    public static partial class Vector3Extensions {
+
+        /// <summary>
+        /// Z成分を固定したベクトルを取得する
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static Vector3 XY(this Vector3 src, float z = 0f)
+        {
+            Vector3 dst = src;
+            dst.z = z;
+            return dst;
+        }
+
+        /// <summary>
+        /// Y成分を固定したベクトルを取得する
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static Vector3 XZ(this Vector3 src, float y = 0f)
+        {
+            Vector3 dst = src;
+            dst.y = y;
+            return dst;
+        }
+
+        /// <summary>
+        /// X成分を固定したベクトルを取得する
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static Vector3 YZ(this Vector3 src, float x = 0f)
+        {
+            Vector3 dst = src;
+            dst.x = x;
+            return dst;
+        }
+    }
+}
